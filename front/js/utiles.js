853156de030cfc6e99324a387c5d2a3a6025bae4
juan carlos ojeda <juancarlos.ojedagomez@mercadolibre.com>
2019-05-07 23:23:15 -0300

    added nav to front

---
+++ front/js/utiles.js
@@ -0,0 +1,9 @@
+function changeDivContent() { 
+    document.getElementById("nav").innerHTML = "<nav class='nav nav-pills nav-justified'>"+
+    "<a class='nav-item nav-link' href='#'><ion-icon size='large' name='paper-plane'></ion-icon></a>"+
+    "<a class='nav-item nav-link' href='#'><ion-icon size='large' name='logo-buffer'></ion-icon></a>"+
+    "<a class='nav-item nav-link' href='#'><ion-icon size='large' name='contacts'></ion-icon></a>"+
+"</nav>";
+};
+
+changeDivContent()
